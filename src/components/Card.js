cdc58bbf6163c57f81bffe3c0979f805a161e2a4
Marcus Bass <Bass.Marcus@icloud.com>
2019-04-16 20:13:50 -0700

    Card component added and styled

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,14 @@ import React, { Component } from 'react'
 export default class Card extends Component {
   render() {
     return (
+      <div className="profileCard">
       <div>
-        <img alt="" src="https://placehold.it/75"/>
-        <div>Name here...</div>
+      <img alt="" src="https://placehold.it/75"/>
+      </div>
+      <div className="profileDetails">
+      <div>Name here...</div>
         <div>Company here...</div>
+      </div>
       </div>
     )
   }
