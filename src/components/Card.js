983e19513a58f278cc1f0c55f0bae950932a8131
Marcus Bass <Bass.Marcus@icloud.com>
2019-04-16 20:04:00 -0700

    Card component added

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+  render() {
+    return (
+      <div>
+        <img alt="" src="https://placehold.it/75"/>
+        <div>Name here...</div>
+        <div>Company here...</div>
+      </div>
+    )
+  }
+}
